811b9e8213268603c7590dbb9f306c2967f52785
alexMuhin <myx87@bk.ru>
2024-08-11 21:27:48 +0300

    заздание Todolist проекта

---
+++ sprint1/1-todolist/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todolist";
+
+
+type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+
+function App() {
+    const tasks1: Array<TaskType> = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'ReactJS', isDone: false}
+    ]
+    const tasks2: Array<TaskType> = [
+        {id: 1, title: 'Hello world', isDone: true},
+        {id: 2, title: 'I am Happy', isDone: false},
+        {id: 3, title: 'Yo', isDone: false}
+    ]
+
+    return (
+        <div className="App">
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'Songs'} tasks={tasks2}/>
+            {/*<Todolist title={'Books'}/>*/}
+        </div>
+    );
+}
+
+export default App;
---
+++ sprint1/1-todolist/src/Todolist.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+type PropsType = {
+    title: string
+    tasks: TaskType[]
+}
+
+export const Todolist = ({title, tasks}: PropsType) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
+                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+};
+
